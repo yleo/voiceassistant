0e2c53f37c1b2e4b73c36cec9673903874d18af6
Yannick Léo <yannick.leo@emerton-data.com>
2024-05-26 23:16:28 +0200

    add api

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def send_to_api(file_path):
         return {"error": f"Failed to get a valid response: {response.status_code}, {response.text}"}
 
 # Record audio
-st.title("Voice Recorder c")
+st.title("Voice Recorder V")
 audio_bytes = audio_recorder()
 
 if audio_bytes:
