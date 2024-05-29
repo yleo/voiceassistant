154c8e8f1197806a7d215e1cf1e0bdcc7651efdc
Yannick Léo <yannick.leo@emerton-data.com>
2024-05-29 16:01:59 +0200

    tts

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import requests
+import requests
 import tempfile
-import base64
+import numpy as np
+import wave
+import io
 
 from groq import Groq
 
@@ -54,7 +57,7 @@ if audio_bytes:
         messages=[
             {
                 "role": "user",
-                "content": "Answer to this query in less thant 20 words as you are a kind voice assistant. The query: Who is instein?",
+                "content": "Answer to this query in less thant 20 words as you are a kind voice assistant. The query: "+result['text'],
             }
         ],
         model="llama3-8b-8192",
@@ -62,5 +65,55 @@ if audio_bytes:
 
     st.write("API Response:", chat_completion.choices[0].message.content)
 
+    answer = chat_completion.choices[0].message.content
+
+    # Define the URL and the payload
+    url = "https://yh2m49z3xrn07uzh.us-east-1.aws.endpoints.huggingface.cloud"
+    payload = {
+        "inputs": answer,
+        "voice_description": "A male speaker with a low-pitched voice speaks slightly fast."
+    }
+
+    # Define the headers, including your Authorization token
+    headers = {
+        "Authorization": "Bearer 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",
+        "Content-Type": "application/json"
+    }
+
+    # Make the POST request
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    # Check the response
+    if response.status_code == 200:
+        print("Success!")
+        print("Response JSON:", response.json())
+    else:
+        print("Failed with status code:", response.status_code)
+        print("Response text:", response.text)
+        
+    # Load audio and save
+    json_response = json.dumps(response.json()[0])
+
+
+    # Parse the JSON response
+    data = json.loads(json_response)
+    audio_samples = data['generated_audio']
+
+    audio_array = np.array(audio_samples, dtype=np.float32)
+    audio_int16 = (audio_array * 32767).astype(np.int16)
+
+    audio_buffer = io.BytesIO()
+    with wave.open(audio_buffer, 'wb') as wf:
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(22050)
+        wf.writeframes(audio_int16.tobytes())
+
+    audio_buffer.seek(0)
+
+    st.title("Audio Playback in Streamlit")
+    st.write("Generated Audio:")
+
+    st.audio(audio_buffer, format='audio/wav')
 
 
