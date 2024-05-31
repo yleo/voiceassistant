77859b0c9a6f375077f72dfce02814f892c115c3
Yannick Léo <yannick.leo@emerton-data.com>
2024-05-31 17:28:28 +0200

    funciton calls

--- app.py
+++ app.py
@@ -43,19 +43,105 @@ def process_audio(audio_bytes):
     
     return result
 
-# Function to interact with LLM
-def get_llm_response(query_text):
-    client = Groq(api_key=LLM_API_KEY)
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": f"Répondez à cette question en moins de 20 mots en tant que gentil assistant vocal. La question:  {query_text}",
-            }
-        ],
-        model="llama3-8b-8192",
+def get_conference_data(info_type='information_confs'):
+    """btenir une brève description de la conférence ou des informations sur le déroulement, les intervenants des différentes conférences en fonction de l'argument info_type.
+
+    Args:
+        info_type (str): Type d'information à récupérer ('description' ou 'information_confs').
+
+    Returns:
+        str: Chaîne JSON de l'information demandée sur la conférence.
+    """
+    # Load JSON data
+    with open('data/conference_data.json', encoding='utf-8') as f:
+        conference_data = json.load(f)
+
+    if info_type == 'description':
+        return json.dumps(conference_data['description'], ensure_ascii=False)
+    else:
+        return json.dumps(conference_data['timeline'], ensure_ascii=False)
+
+def get_llm_response(user_prompt):
+    system_prompt = '''
+    Tu es un modèle de langage d'appel de fonction, et tu es chargé de répondre à des questions concernant la conférence EMERTON : Le vrai enjeu de l'IA générative lors du DATA & AI for Business Forum and Meetings.
+    Ces informations sont stockées dans un fichier JSON avec une description de la conférence et un planning des différentes sessions.
+    Les deux sections sont accessibles via les clés 'description' et 'timeline' respectivement.
+    Étant donné une question d'un utilisateur sur la conférence, détermine le type d'information à récupérer.
+    Si la question porte sur une présentation de la conférence, appelle get_conference_data avec l'argument 'description'.
+    Pour toutes les autres questions pouvant notamment porter sur les intervenants, les horaires, les titres des conférences, appelle get_conference_data avec l'argument 'information_confs'.
+    Si tu n'es pas en mesure de répondre à la question de l'utilisateur, appelle get_conference_data avec l'argument 'information_confs' pour obtenir des informations générales sur la conférence.
+
+    TA REPONSE FINALE DOIT ETRE COURTE, CONCISE ET EN FRANCAIS.
+    '''
+
+    client = Groq()
+    MODEL = 'llama3-70b-8192'
+
+    messages = [
+        {
+            "role": "system",
+            "content": system_prompt
+        },
+        {
+            "role": "user",
+            "content": user_prompt,
+        }
+    ]
+    tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": "get_conference_data",
+                "description": "Obtenir une brève description de la conférence ou des informations sur le déroulement, les intervenants des différentes conférences en fonction de l'argument info_type.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {},
+                    "required": [],
+                },
+            },
+        },
+    ]
+    
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=messages,
+        tools=tools,
+        tool_choice="auto",
+        max_tokens=4096
     )
-    return chat_completion.choices[0].message.content
+
+    response_message = response.choices[0].message
+    tool_calls = response_message.tool_calls
+    print(f"Tool calls: {tool_calls}")
+    if tool_calls:
+        available_functions = {
+            "get_conference_data": get_conference_data,
+        }
+        messages.append(response_message)
+        for tool_call in tool_calls:
+            function_name = tool_call.function.name
+            function_to_call = available_functions[function_name]
+            function_args = json.loads(tool_call.function.arguments)
+            try:
+                function_response = function_to_call(**function_args)
+            except Exception :
+                function_args = {'info_type': 'information_confs'}
+                function_response = function_to_call(**function_args)
+
+            messages.append(
+                {
+                    "tool_call_id": tool_call.id,
+                    "role": "tool",
+                    "name": function_name,
+                    "content": function_response,
+                }
+            )
+        second_response = client.chat.completions.create(
+            model=MODEL,
+            messages=messages,
+            max_tokens=4096
+        )
+    return second_response.choices[0].message.content
 
 # Function to generate audio response
 def generate_audio_response(answer):
